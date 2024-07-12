ea905d65c34dee9087e6919319cce70bfb6f1895
Gashugi1 <miguel.gashugi@strathmore.edu>
2024-07-12 07:19:59 +0300

    Added BottomNavigation widget: Custom bottom navigation bar with notification count display

---
+++ lib/screens/bottom_navigation.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+  final int notificationCount;
+  final BuildContext context;
+
+  BottomNavigation({
+    required this.currentIndex,
+    required this.onTap,
+    required this.context,
+    this.notificationCount = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Color(0xFF8DBAC4),
+      currentIndex: currentIndex,
+      onTap: (index) {
+        onTap(index);
+      },
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Stack(
+            children: [
+              Icon(Icons.notifications),
+              if (notificationCount > 0)
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(1),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 12,
+                      minHeight: 12,
+                    ),
+                    child: Text(
+                      '$notificationCount',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 8,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+            ],
+          ),
+          label: 'Notice',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+      selectedItemColor: Colors.grey,
+    );
+  }
+}
